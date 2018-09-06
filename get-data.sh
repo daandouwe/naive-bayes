d0d986fafd6b618937824303ec8d434a31a1face
daandouwe <daan_douwe@outlook.com>
2018-09-06 19:29:54 +0200

    Download data.

--- get-data.sh
+++ get-data.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-mkdir -p data
+mkdir -p data/classes
+mkdir -p data/binary
 
-python make-binary-classes.py ~/data/neubig-data/classes/train.txt data/train.txt
-python make-binary-classes.py ~/data/neubig-data/classes/dev.txt data/dev.txt
-python make-binary-classes.py ~/data/neubig-data/classes/test.txt data/test.txt
+wget https://raw.githubusercontent.com/neubig/nn4nlp-code/master/data/classes/train.txt
+wget https://raw.githubusercontent.com/neubig/nn4nlp-code/master/data/classes/test.txt
+wget https://raw.githubusercontent.com/neubig/nn4nlp-code/master/data/classes/dev.txt
+
+mv train.txt dev.txt test.txt data/classes
+
+python make-binary-classes.py data/classes/train.txt data/binary/train.txt
+python make-binary-classes.py data/classes/dev.txt data/binary/dev.txt
+python make-binary-classes.py data/classes/test.txt data/binary/test.txt
